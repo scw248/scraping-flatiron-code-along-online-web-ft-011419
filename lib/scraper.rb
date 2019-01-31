4ccc5499c4096ff45fabe99d675aa13b6d12ee37
Stephen Wright <scw248@gmail.com>
2019-01-31 15:04:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,7 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
   
     def print_courses
@@ -23,5 +24,6 @@ class Scraper
   
 end
 
+Scraper.new.get_page
 
 
